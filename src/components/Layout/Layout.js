5281ce9c76656a60abc8ad5e01e2075a22886c3c
bela-dev <fori-prg@pathwwway.com>
2019-01-04 13:41:03 +0200

    toggle sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-  <>
-    <Toolbar />
-    <SideDrawer />
-    <main className={styles.Content}>
-      {props.children}
-    </main>
-  </>
-);
+class Layout extends Component {
 
-export default layout;
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}  />
+        <SideDrawer 
+          open={this.state.showSideDrawer} 
+          closed={this.sideDrawerClosedHandler} 
+        />
+        <main className={styles.Content}>
+          {this.props.children}
+        </main>
+      </>
+    );
+  }
+}
+
+export default Layout;
